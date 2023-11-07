0351d24f59d388a778e6550dc37dfeb42d5ba43c
h-suo <phstoo1021@naver.com>
2023-11-07 20:46:34 +0900

    ✨Feat: BOJ13300 방배정 문제 정리

---
+++ Algorithm/Algorithm/BOJ13300.swift
@@ -0,0 +1,46 @@
+//
+//  BOJ13300.swift
+//  Algorithm
+//
+//  Created by 표현수 on 2023/11/07.
+//  방배정: https://www.acmicpc.net/problem/13300
+//
+
+// MARK: 풀이 1
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+
+var studentList = Array(repeating: 0, count: 12)
+
+for _ in 1...input[0] {
+    let studentInfo = readLine()!.split(separator: " ").map { Int($0)! }
+    let num = 2 * studentInfo[1] - 1
+    
+    studentList[num - studentInfo[0]] += 1
+}
+
+let result = studentList.reduce(0) {
+    let romCount = ceil(Double($1) / Double(input[1]))
+    
+    return $0 + Int(romCount)
+}
+
+print(result)
+
+// MARK: 풀이 2
+//let input = readLine()!.split(separator: " ").map { Int($0)! }
+//
+//var studentList: [String: Int] = [:]
+//
+//for _ in 1...input[0] {
+//    let studentInfo = readLine()!
+//
+//    studentList[studentInfo, default: 0] += 1
+//}
+//
+//let result = studentList.values.reduce(0) {
+//    $0 + (($1 + input[1] - 1) / input[1])
+//}
+//
+//print(result)
